3271ea2d6c8099fdc7076aea89771228193b5609
Luan-CortesZ <luan.corteszuka@gmail.com>
2026-02-25 11:24:48 +0100

    Init project structure

--- pf-frontend/src/app/main.tsx
+++ pf-frontend/src/app/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.tsx'
+import './views/styles/index.scss'
+import App from './views/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ pf-frontend/src/app/views/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
+import './styles/App.scss'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div>
+        PokéFinder
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
