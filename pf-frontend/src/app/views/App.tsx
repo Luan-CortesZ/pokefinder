640bebb304d1349b4ed176c3ff08097bd2f95062
Luan-CortesZ <luan.corteszuka@gmail.com>
2026-02-26 13:10:40 +0100

    Create some features

--- pf-frontend/src/app/views/App.tsx
+++ pf-frontend/src/app/views/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import './styles/App.scss'
+import HomePage from '../../features/HomePage/views/HomePage'
+import Header from '../../shared/components/Header/Header'
 
 function App() {
 
   return (
     <BrowserRouter>
-      <div>
-        PokéFinder
-      </div>
+      <Header />
+      <HomePage />
     </BrowserRouter>
   )
 }
