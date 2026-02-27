42b69e1712fcb7e3ad3df13693b00c5f188d5bfe
Luan-CortesZ <luan.corteszuka@gmail.com>
2026-02-27 21:56:10 +0100

    Finish footer

--- pf-frontend/src/shared/components/Footer/Footer.tsx
+++ pf-frontend/src/shared/components/Footer/Footer.tsx
@@ -2,6 +2,34 @@ import './styles/Footer.scss'
 export default function Footer() {
     return (
      <footer className="general-footer">
+        <div className="button">
+					<span className="front">
+					</span>
+        </div>
+        <div className="green-screen">
+
+        </div>
+        <div className="dpad-container">
+					<div className="pushable up">
+							<span className="front">▲</span>
+					</div>
+					
+					<div className="pushable left">
+							<span className="front">◀</span>
+					</div>
+
+					<div className="pushable right">
+							<span className="front">▶</span>
+					</div>
+
+					<div className="pushable down">
+							<span className="front">▼</span>
+					</div>
+					
+					<div className="dpad-center">
+						<span className="front"></span>
+					</div>
+				</div>
      </footer>
     );
 }
