381c48173c47e14759a1290da285c7781452d080
Luan-CortesZ <luan.corteszuka@gmail.com>
2026-03-28 12:43:59 +0100

    configure backend caching

--- pf-frontend/src/services/pokemon.service.ts
+++ pf-frontend/src/services/pokemon.service.ts
@@ -5,14 +5,9 @@ const API_URL = 'http://localhost:3000/api/pokemon';
 export const PokemonService = {
     getPokemonsByRegion: async (regionId: string): Promise<Pokemon[]> => {
         try {
-            const cachedData = localStorage.getItem(`pokemons-${regionId}`);
-            if (cachedData) {
-                return JSON.parse(cachedData);
-            }
             const response = await fetch(`${API_URL}/region/${regionId}`);
             if (!response.ok) throw new Error('Erreur réseau');
             const pokemons = await response.json();
-            localStorage.setItem(`pokemons-${regionId}`, JSON.stringify(pokemons));
             return pokemons;
         } catch (error) {
             console.error("Erreur service:", error);
